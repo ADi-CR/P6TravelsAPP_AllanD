5d6c1dbc979262dd5b503f6502dc83ed527ce8c6
Allan Delgado C <allandelgadoc@gmail.com>
2024-07-04 21:02:31 -0600

    Add project files.

---
+++ P6TravelsAPP_AllanD/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using P6TravelsAPP_AllanD.Views;
+
+namespace P6TravelsAPP_AllanD
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new LoginPage());
+        }
+    }
+}
---
+++ P6TravelsAPP_AllanD/Views/LoginPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace P6TravelsAPP_AllanD.Views;
+
+public partial class LoginPage : ContentPage
+{
+	public LoginPage()
+	{
+		InitializeComponent();
+	}
+}
